1873801e1f6cba48d4fa4bdc951b4734af5b1de5
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-16 17:49:02 -0600

    second update It2

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,29 @@
 require './lib/board'
 require './lib/place_user_input'
+require './lib/place_computer_input'
 
 puts "I have generated Connect-Four for you, to place your piece, you will just need to enter the letter (A-G) of the column you want to place your piece at"
 
 start = GameBoard.new
 start.print_board
-turn = PlaceUserInput.new(start)
-user_plays = turn.place_your_piece
-played_column = turn.identify_user_input_position(user_plays)
-place_input = turn.place_user_input(played_column)
-updating_board = turn.update_board(place_input)
-print_new_board = turn.print_updated_board
+
+user_turn = PlaceUserInput.new(start)
+
+user_plays = user_turn.place_your_piece
+
+users_column = user_turn.identify_user_input_position(user_plays)
+
+place_users_input = user_turn.place_user_input(users_column)
+
+update_user_board = user_turn.update_user_input(place_users_input)
+
+print_users_board = user_turn.print_user_board
+
+computer_turn = PlaceComputerInput.new(user_turn)
+
+computer_plays = computer_turn.computer_places_piece
+
+update_computer_board = computer_turn.update_computer_input(computer_plays)
+
+print_computers_board = computer_turn.print_computer_board
+
